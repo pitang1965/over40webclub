5e5a9802d5c8d7f4e4cf010439c74c4e791db381
Shoji Makino <pitang1965@gmail.com>
2020-09-29 20:39:55 +0900

    initial commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,27 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Over 40 Web Club', // e.g: 'Name | Developer'
+  lang: 'jp', // e.g: en, es, fr, jp
+  description: '40代以上のみなさまへ', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'こちらは',
+  name: 'Over 40 Web Club',
+  subtitle: 'Web開発の勉強をしている40歳以上の方のためのオンラインコミュニティです。',
+  cta: 'より詳しく',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'icon-512x512.png',
+  paragraphOne:
+    'Web開発の勉強をしている40歳以上の方が、情報交換したり、助けあったり、交流を深めりするためのオンラインコミュニティです。',
+  paragraphTwo: '無料で非公開となっています。',
+  paragraphThree: '趣旨をご理解いただける方は誰でも加入できます。',
+  resume: 'https://software.pitang1965.com/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,37 +30,38 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: '中高年専門のコミュニティ',
+    info: '20代、30代の方が集まるコニュニティとは違います。',
+    info2: '皆様の自己紹介が濃く、時に苦く、ちょっとジーンときてしまうことも。',
+    url: 'https://software.pitang1965.com/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: '主なチャンネル',
+    info: '昨日できたばかりなので随時改良していきますが、現在以下のチャンネルがございます。',
+    info2:
+      '自己紹介、今日やること、将来の夢、質問、学習報告、好きな音楽、雑談、うまくいかなかった報告・・・',
+    url: 'https://software.pitang1965.com/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: '当クラブのルール',
+    info: '技術を競い合ったりマウントするのではなく・・・',
+    info2: '興味を持ったことをじっくり学べるようお互いにサポートしていきます。',
+    url: 'https://software.pitang1965.com/',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: '私達のコミュニティに入りたいですか? いいですね!',
+  btn: 'ご連絡をお待ちしています',
+  email: 'http://www.twitter.com/messages/compose?recipient_id=59996143',
 };
 
 // FOOTER DATA
@@ -68,22 +70,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/pitang1965',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/pitang1965',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/pitang1965',
     },
   ],
 };
